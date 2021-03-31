c316565f272e637003f0cc98fcd438826d851e9f
wyatth7 <whardin1010@gmail.com>
2021-03-30 22:46:31 -0400

    set up gitignore

--- server/dist/utils/errHandler.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sendRes = void 0;
-const sendRes = (res, code, message) => {
-    return res.status(code).json({ message });
-};
-exports.sendRes = sendRes;
-exports.default = exports.sendRes;
