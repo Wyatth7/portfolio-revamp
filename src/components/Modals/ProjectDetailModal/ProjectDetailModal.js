6b0665919ff83c1564e2e79a2443c13fb4a4e5b4
wyatth7 <whardin1010@gmail.com>
2022-08-02 18:56:55 -0400

    Added mobile modal enhancements.

--- src/components/Modals/ProjectDetailModal/ProjectDetailModal.js
+++ src/components/Modals/ProjectDetailModal/ProjectDetailModal.js
@@ -1,36 +1,48 @@
 import React from "react";
 import Modal from "../Modal";
-import {faGlobe} from  "@fortawesome/free-solid-svg-icons";
-import {faGithub} from  "@fortawesome/free-brands-svg-icons";
+import { faGlobe } from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import ProjectLink from "../../Modals/ProjectDetailModal/ProjectLink/ProjectLink";
 import useStore from "../../../custom-hooks/useStore";
 
-
 const ProjectDetailModal = (props) => {
-    const project = useStore()[0].selectedProject;
-    return (
-        <Modal title={project.title}>
-            <div className="ProjectDetailModal">
-                <p className="description project-text">{project.description}</p>
-                <div className="tag-container">
-                    <h2 className="sub-modal-header">Powered By</h2>
-                    <div className="tags">
-                        { project.tags ?
-                            project.tags.map((tag, index) => (
-                            <p key={index} className="tag project-text">{tag}</p>
-                        )) : null }
-                    </div>
-                </div>
-                <div className="project-link-container">
-                    <h2 className="sub-modal-header">Check It Out</h2>
-                    <div className="project-links">
-                        <ProjectLink icon={faGlobe} text="View Website" link={project.link} />
-                        <ProjectLink icon={faGithub} text="View Code" link={project.github} />
-                    </div>
-                </div>
+  const project = useStore()[0].selectedProject;
+  return (
+    <Modal title={project.title}>
+      <div className="ProjectDetailModal">
+        <p className="description project-text">{project.description}</p>
+        <div className="detail-container">
+          <div className="tag-container">
+            <h2 className="sub-modal-header">Powered By</h2>
+            <div className="tags">
+              {project.tags
+                ? project.tags.map((tag, index) => (
+                    <p key={index} className="tag project-text">
+                      {tag}
+                    </p>
+                  ))
+                : null}
             </div>
-        </Modal>
-    );
-}
+          </div>
+          <div className="project-link-container">
+            <h2 className="sub-modal-header">Check It Out</h2>
+            <div className="project-links">
+              <ProjectLink
+                icon={faGlobe}
+                text="View Website"
+                link={project.link}
+              />
+              <ProjectLink
+                icon={faGithub}
+                text="View Code"
+                link={project.github}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </Modal>
+  );
+};
 
 export default ProjectDetailModal;
--- src/custom-hooks/useStore.js
+++ src/custom-hooks/useStore.js
@@ -1,29 +1,29 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
 let globalState = {
-    showModal: false,
-    selectedProject: {}
+  showModal: false,
+  selectedProject: {},
 };
 let actions = {
-    SHOW_MODAL_DISPATCH: (state, payload) => {
-        return {showModal: !state.showModal}
-    },
-    // payload for this method must be formatted on state update.
-    SELECTED_PROJECT: (state, payload) => {
-        return {selectedProject: {...payload}}
-    }
+  SHOW_MODAL_DISPATCH: (state, payload) => {
+    return { showModal: !state.showModal };
+  },
+  // payload for this method must be formatted on state update.
+  SELECTED_PROJECT: (state, payload) => {
+    return { selectedProject: { ...payload } };
+  },
 };
 
 const useStore = () => {
-    const setState = useState(globalState)[1];
-    
-    const dispatch = (action, payload = null) => {
-        const newState = actions[action](globalState, payload);
-        globalState = {...globalState, ...newState};
-        setState(globalState);
-    }
-    
-    return [globalState, dispatch];
+  const setState = useState(globalState)[1];
+
+  const dispatch = (action, payload = null) => {
+    const newState = actions[action](globalState, payload);
+    globalState = { ...globalState, ...newState };
+    setState(globalState);
+  };
+
+  return [globalState, dispatch];
 };
 
 export default useStore;
